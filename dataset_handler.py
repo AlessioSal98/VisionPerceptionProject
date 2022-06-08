d6cbc760f26e8d57c19cc8038ec53b7916b38b00
AlessioSal98 <48289849+AlessioSal98@users.noreply.github.com>
2022-06-08 10:19:44 +0200

    Add files via upload

--- dataset_handler.py
+++ dataset_handler.py
@@ -3,7 +3,6 @@ This class is used for loading the videos of the dataset and store them as torch
 '''
 
 import torch
-#import utils
 from tqdm import tqdm
 from random import sample
 import os
@@ -68,21 +67,6 @@ def load(name):
   with open(name, 'rb') as f:
     array = np.load(f)
   return array
-
-'''
-def split(X,y,percentage):
-  if(X.shape[0]==y.shape[0]):
-    perm = np.random.permutation(len(X))
-    X=X[perm]
-    y=y[perm]
-    first_half = int(X.shape[0]*(1-percentage))
-
-    X_1 = X[0:first_half]
-    y_1 = y[0:first_half]
-    X_2 = X[first_half:X.shape[0]]
-    y_2 = y[first_half:y.shape[0]]
-  return X_1, y_1 , X_2, y_2
-'''
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Dataset Parameters')
@@ -154,15 +138,3 @@ if __name__ == "__main__":
     video_labels = video_labels.cpu().detach().numpy()
 
     unique, counts = np.unique(video_labels, return_counts=True)
-
-    '''
-    X_train, y_train , X_test, y_test = split(video_names,video_labels,0.2)
-    X_train, y_train , X_val, y_val = split(X_train,y_train,0.3)
-
-    save(X_train,save_path+'X_train.npy')
-    save(y_train,save_path+'y_train.npy')
-    save(X_val,save_path+'X_val.npy')
-    save(y_val,save_path+'y_val.npy')
-    save(X_test,save_path+'X_test.npy')
-    save(y_test,save_path+'y_test.npy')
-    '''
